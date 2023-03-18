67119039ce9113f6dc6fec4e9ac9be49713d6b85
zacharyvincze <zacharyvincze@gmail.com>
2023-03-18 17:00:54 -0400

    Clean scraped text

--- company_data_scraper.py
+++ company_data_scraper.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import requests
 import sys
 import urllib.parse
@@ -6,6 +7,7 @@ import re
 import urllib
 from pdfreader import SimplePDFViewer
 import os
+import logging
 
 def download_url(filename, url):
     r = requests.get(url)
@@ -37,14 +39,29 @@ def extract_pdf_text(filepath):
     print("Extracted text from: " + filepath)
     return result
 
+def extract_relevant_blocks(filename, keywords):
+    with open(filename, "r") as fd:
+        content = fd.read()
+        blocks = re.findall(r'\.([^\\.]+\b{}\b[^\\.]*.)'.format(keywords), content, re.IGNORECASE)
+
+        return blocks
+
 if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.ERROR)
+
+    # Inputs
     company_name = sys.argv[1]
-    keywords = "sustainability report"  # Change this to be configurable?
+    search_keywords = "sustainability report"
+    text_extraction_keywords = ["green"]
+
+    # Ouputs
+    pipeline_output = []
+
+    # Implementation
     
-    query = urllib.parse.quote_plus(company_name + " " + keywords)
+    query = urllib.parse.quote_plus(company_name + " " + search_keywords)
 
     url = "https://www.google.com/search?q=filetype:pdf+" + query
-    print(url)
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     result_block = soup.find_all("a", href=True)
@@ -65,5 +82,10 @@ if __name__ == "__main__":
             download_url(base_output_filename + ".pdf", pdf_url)
             extracted_text = extract_pdf_text(base_output_filename + ".pdf")
             write_string_to_file(base_output_filename + ".txt", extracted_text)
-            
+
+            for text_extraction_keyword in text_extraction_keywords:
+                pipeline_output += extract_relevant_blocks(base_output_filename + ".txt", text_extraction_keyword)
+
             cur_result += 1
+
+    print(pipeline_output)
