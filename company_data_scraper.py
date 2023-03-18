ce98ceb803a6e5c6d3172a7929aa3bdb39b6fb73
zacharyvincze <zacharyvincze@gmail.com>
2023-03-18 15:17:44 -0400

    Add pdf text extraction

--- company_data_scraper.py
+++ company_data_scraper.py
@@ -4,17 +4,42 @@ import urllib.parse
 from bs4 import BeautifulSoup
 import re
 import urllib
+from pdfreader import SimplePDFViewer
+import os
 
 def download_url(filename, url):
     r = requests.get(url)
     if (r.status_code == 200):
         output_file = open(filename, "wb")
         output_file.write(r.content)
+        output_file.close()
         print("Downloaded: " + url)
+
+def write_string_to_file(filename, content):
+    fd = open(filename, "w")
+    fd.write(content)
+    fd.close()
+    print("Wrote data to " + filename)
+
+def extract_pdf_text(filepath):
+    fd = open(filepath, "rb")
+    viewer = SimplePDFViewer(fd)
+    
+    result = ""
+
+    # Iterate through all document pages and extract text
+    for canvas in viewer:
+        for token in canvas.strings:
+            result += token
+    
+    fd.close()
+    
+    print("Extracted text from: " + filepath)
+    return result
 
 if __name__ == "__main__":
     company_name = sys.argv[1]
-    keywords = "sustainability report"
+    keywords = "sustainability report"  # Change this to be configurable?
     
     query = urllib.parse.quote_plus(company_name + " " + keywords)
 
@@ -24,14 +49,21 @@ if __name__ == "__main__":
     soup = BeautifulSoup(r.text, "html.parser")
     result_block = soup.find_all("a", href=True)
 
-    iterator = 0
+    cur_result = 0
 
     for result in result_block:
         href = result['href']
         pdf_href = re.search(r'(\/url\?q\=)(.+\b\.pdf\b)', href)
         
+        # Check if this link contains a downloadable pdf file
         if pdf_href:
             pdf_url = pdf_href.group(2)
-            download_url("result" + str(iterator) + ".pdf", pdf_url)
-            iterator += 1
-        
+
+            os.makedirs("tmp", exist_ok=True)
+            base_output_filename = "tmp/result_" + str(cur_result)
+            
+            download_url(base_output_filename + ".pdf", pdf_url)
+            extracted_text = extract_pdf_text(base_output_filename + ".pdf")
+            write_string_to_file(base_output_filename + ".txt", extracted_text)
+            
+            cur_result += 1
